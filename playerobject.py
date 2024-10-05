0a6f79cba5d03df65e7ba674df0642959e098d31
tai-shis <tshis070@mtroyal.ca>
2024-10-05 13:34:11 -0600

    Implemented custom Goal class in Player class

--- playerobject.py
+++ playerobject.py
@@ -1,36 +1,41 @@
+class Goal:
+        def __init__(self, goal: str) -> None:
+            self.goal = goal
+            self.is_comleted = False #default to false, can change to true
+
 class Player:
     def __init__(self, name: str) -> None:
         self.name = name  
-        self.small_goals = None
-        self.medium_goals = None
-        self.large_goals = None
+        self.small_goals = []
+        self.medium_goals = []
+        self.large_goals = []
         self.score = 0
         self.play_time = 0
 
+    def goals_to_goals(goals: list[str]) -> list[Goal]:
+        """
+        Converts list of goal strings into list of custom goal objects,
+        see Goal class for more information
+        """
+        goal_list = []
+        for goal in goals:
+            goal_list.append(Goal(goal))
+
+        return goal_list
+
     # setters
-    def add_small_goals(self, goals: str) -> None:
-        self.small_goals = goals
+    def set_small_goals(self, goals: list) -> None:
+        self.small_goals = self.goals_to_goals(goals)
 
-    def add_medium_goals(self, goals: str) -> None:
-        self.medium_goals = goals
 
-    def add_large_goals(self, goals: str) -> None:
-        self.large_goals = goals
+    def set_medium_goals(self, goals: list) -> None:
+        self.medium_goals = self.goals_to_goals(goals)
 
-    # getters for each 
-    def get_small_goals(self) -> list:
-        return self.small_goals
-    
-    def get_medium_goals(self) -> list:
-        return self.medium_goals
-    
-    def get_large_goals(self) -> list:
-        return self.large_goals
-
+    def set_large_goals(self, goals: list) -> None:
+        self.large_goals = self.goals_to_goals(goals)
 
     def __str__(self) -> str:
         return (f"Player: {self.name}\n" + 
-                f"small goals: {self.small_goals}\n" +
-                f"medium goals: {self.medium_goals}\n" +
-                f"large goals: {self.large_goals}\n")
-
+                f"Small goals: {self.small_goals}\n" +
+                f"Medium goals: {self.medium_goals}\n" +
+                f"Large goals: {self.large_goals}\n")
