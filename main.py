581ae3f1db564a5c6c19d9b5bd244ee973ab4aa0
SubterraGuy <josenari12@gmail..com>
2024-10-05 15:08:33 -0600

    logic fixing for input 1

--- main.py
+++ main.py
@@ -15,23 +15,33 @@ small_goal_list = []
 medium_goal_list = []
 large_goal_list = []
 
+
+
 # Get goals from user input
-while(user_input_small == "" or user_input_small != "done" or user_input_small == "none"):
-    small_goal = input("PLease put in your small goals: ")
+small_goal = input("PLease put in your small goals: ")
+if(isinstance(small_goal, str)):
     small_goal_list.append(small_goal)
+    
+    while(user_input_small != "" or user_input_small != "done"):
+        small_goal = input("PLease put in your small goals: ")
+        small_goal_list.append(small_goal)
     player1.goals_to_goals(small_goal_list)
 
-    
-while(user_input_medium == "" or user_input_medium != "done" or user_input_medium == "none"):
+
+medium_goal = input("PLease put in your medium goals: ")
+while(user_input_medium != "" or user_input_medium != "done"):
     medium_goal = input("PLease put in your medium goals: ")
     medium_goal_list.append(medium_goal)
-    player1.golas_to_goals(medium_goal_list)
+    
+player1.golas_to_goals(medium_goal_list)
     
     
-while(user_input_large == "" or user_input_large != "done" or user_input_large == "none"):
+large_goal = input("PLease put in your large goals: ")
+while(user_input_large != "" or user_input_large != "done"):
     large_goal = input("PLease put in your large goals: ")
     large_goal_list.append(large_goal)
-    player1.goals_to_goals(large_goal_list)
+    
+player1.goals_to_goals(large_goal_list)
     
 
 
