71f88a18c334015dd22f7978be30efc427d95955
tai-shis <tshis070@mtroyal.ca>
2024-10-05 22:05:12 -0600

    Implement ball spawn method

--- ball.py
+++ ball.py
@@ -1,4 +1,8 @@
 import pygame
+from random import randint
+
+def loadify(img_name):
+    return pygame.image.load(img_name).convert_alpha()
 
 class Ball(pygame.sprite.Sprite):
     def __init__(self, start_position : tuple[int,int], launch_angle : int) -> None:
@@ -9,10 +13,19 @@ class Ball(pygame.sprite.Sprite):
 
         self.current_angle = launch_angle
 
+        self.velocity = [0, 0] #initial velocity at zero and spawning the ball will set one
+
+        self.image = pygame.transform.smoothscale(loadify('Bullet.png').size)
+        self.image_orgin = self.image
+        self.rect = self.image.get_rect(center=self.position)
         #de
-    
-    def draw(self) -> None:
-        pass
+
+    def spawn(self):
+        #calculate random position
+        rand_x = randint(8, 500 - 8) # from dimensions with size of sprite included
+        rand_y = randint(8, 500 - 8 )
+        self.position = [rand_x, rand_y]
+        self.velocity = [0.5, 0.5] #arbitrary init velocity going at a 45 deg angle, random later
     
     def move(self) -> None:
         pass
