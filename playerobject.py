d84737f921118e130c14bd476eb3e42167f3c298
tai-shis <tshis070@mtroyal.ca>
2024-10-05 18:41:19 -0600

    fixing __str__ in goal

--- playerobject.py
+++ playerobject.py
@@ -4,7 +4,7 @@ class Goal:
         self.check_status = False 
 
     def __str__(self) -> str:
-        return f"[goal: {self.goal}, is_completed: {self.is_completed}]" 
+        return f"[goal: {self.goal}, check_status: {self.check_status}]" 
 
 class Player:
     def __init__(self, name: str) -> None:
